a3dd070bdd0433dd8e8e144071f4cb4f977d2763
ankit <ankitbisht0205@gmail.com>
2023-07-23 00:04:39 +0530

    adding refs

--- components/project/Projects.tsx
+++ components/project/Projects.tsx
@@ -8,8 +8,10 @@ export default function Projects() {
   const myRef = useRef();
   const secondRef = useRef();
   const { inViewport, leaveCount } = useInViewport(myRef);
-  const { inViewport: second, leaveCount: secondCount } =
-    useInViewport(secondRef);
+  const { inViewport: second, leaveCount: secondCount } =useInViewport(secondRef);
+
+    console.log(second);
+
   return (
     <div id="project" ref={myRef}>
       <div
@@ -157,45 +159,99 @@ export default function Projects() {
       </div>
 
       {/* /////////////////////////////////////////////////////////////////////////////////////////////// */}
-      <div className="relative mt-[200px] max-md:hidden" ref={secondRef}>
-        <div
-          className={`${second && secondCount === 0 && "animate-slideRight1"}`}
-        >
-          <Card
-            gitLink="https://github.com/0205Ankit/userLog"
-            liveLink="https://user-log.vercel.app/"
-            name="User Log"
-            url="/userLog.png"
-            alignRight
-          />
+      <div ref={secondRef} className="max-md:mt-20 mt-[200px] ">
+        <div className="relative max-md:hidden">
+          <div
+            className={`${
+              second && secondCount === 0 && "animate-slideRight1"
+            }`}
+          >
+            <Card
+              gitLink="https://github.com/0205Ankit/userLog"
+              liveLink="https://user-log.vercel.app/"
+              name="User Log"
+              url="/userLog.png"
+              alignRight
+            />
+          </div>
+          <div className="min-w-[50%] max-w-[50%] absolute top-2/4 left-0 -translate-y-2/4 z-10">
+            <p
+              className={`text-violet-500 ${
+                second && secondCount === 0 && "animate-slideLeft1"
+              }`}
+            >
+              Featured Project
+            </p>
+            <h1
+              className={`text-gray-300 text-2xl font-semibold mt-1 ${
+                second && secondCount === 0 && "animate-slideLeft2"
+              }`}
+            >
+              User-Log
+            </h1>
+            <div
+              className={`py-4 px-5 rounded-md bg-darkGrey mt-4 text-gray-300 ${
+                second && secondCount === 0 && "animate-slideLeft3"
+              }`}
+            >
+              A simple CRUD app where u can manage log of users , You can Create
+              , Update , Delete the users
+            </div>
+            <div
+              className={`flex gap-2 tracking-[1.2px] text-gray-300 text-xs mt-4 ${
+                second && secondCount === 0 && "animate-slideLeft4"
+              } `}
+            >
+              <span>React</span>
+              <span>Express</span>
+              <span>PostgreSQL</span>
+              <span>Tailwind</span>
+              <span>Prisma</span>
+              <span>AWS S3</span>
+              <span>Radix UI</span>
+            </div>
+            <div
+              className={`flex gap-3 mt-3 ${
+                second && secondCount === 0 && "animate-slideLeft5"
+              }`}
+            >
+              <a href="https://github.com/0205Ankit/userLog" target="_blank">
+                <LuGithub className="text-gray-300 hover:text-violet-500 text-2xl" />
+              </a>
+              <a href="https://user-log.vercel.app/" target="_blank">
+                <LuLink className="text-gray-300 hover:text-violet-500 text-2xl" />
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="min-w-[50%] max-w-[50%] absolute top-2/4 left-0 -translate-y-2/4 z-10">
+        {/* ///////////////////////////////////////////////////////////////////////////////////////// mobile-screen */}
+        <div className="bg-darkGrey rounded-md py-10 px-10 md:hidden max-[400px]:px-5 ">
           <p
             className={`text-violet-500 ${
-              second && secondCount === 0 && "animate-slideLeft1"
+              second && secondCount === 0 && "animate-slideRight1"
             }`}
           >
             Featured Project
           </p>
           <h1
             className={`text-gray-300 text-2xl font-semibold mt-1 ${
-              second && secondCount === 0 && "animate-slideLeft2"
+              second && secondCount === 0 && "animate-slideRight2"
             }`}
           >
-            User-Log
+            User-log
           </h1>
-          <div
-            className={`py-4 px-5 rounded-md bg-darkGrey mt-4 text-gray-300 ${
-              second && secondCount === 0 && "animate-slideLeft3"
+          <p
+            className={`rounded-md bg-darkGrey mt-4 text-gray-300 text-[15px] ${
+              second && secondCount === 0 && "animate-slideRight3"
             }`}
           >
             A simple CRUD app where u can manage log of users , You can Create ,
             Update , Delete the users
-          </div>
+          </p>
           <div
-            className={`flex gap-2 tracking-[1.2px] text-gray-300 text-xs mt-4 ${
-              second && secondCount === 0 && "animate-slideLeft4"
-            } `}
+            className={`flex gap-2 tracking-[1px] text-gray-300 justify-end text-xs flex-wrap mt-5 ${
+              second && secondCount === 0 && "animate-slideRight4"
+            }`}
           >
             <span>React</span>
             <span>Express</span>
@@ -206,8 +262,8 @@ export default function Projects() {
             <span>Radix UI</span>
           </div>
           <div
-            className={`flex gap-3 mt-3 ${
-              second && secondCount === 0 && "animate-slideLeft5"
+            className={`flex gap-3 justify-end mt-3 ${
+              second && secondCount === 0 && "animate-slideRight5"
             }`}
           >
             <a href="https://github.com/0205Ankit/userLog" target="_blank">
@@ -217,59 +273,6 @@ export default function Projects() {
               <LuLink className="text-gray-300 hover:text-violet-500 text-2xl" />
             </a>
           </div>
-        </div>
-      </div>
-      {/* ///////////////////////////////////////////////////////////////////////////////////////// mobile-screen */}
-      <div
-        className="bg-darkGrey rounded-md py-10 px-10 md:hidden max-[400px]:px-5 mt-20"
-        ref={secondRef}
-      >
-        <p
-          className={`text-violet-500 ${
-            second && secondCount === 0 && "animate-slideRight1"
-          }`}
-        >
-          Featured Project
-        </p>
-        <h1
-          className={`text-gray-300 text-2xl font-semibold mt-1 ${
-            second && secondCount === 0 && "animate-slideRight2"
-          }`}
-        >
-          User-log
-        </h1>
-        <p
-          className={`rounded-md bg-darkGrey mt-4 text-gray-300 text-[15px] ${
-            second && secondCount === 0 && "animate-slideRight3"
-          }`}
-        >
-          A simple CRUD app where u can manage log of users , You can Create ,
-          Update , Delete the users
-        </p>
-        <div
-          className={`flex gap-2 tracking-[1px] text-gray-300 justify-end text-xs flex-wrap mt-5 ${
-            second && secondCount === 0 && "animate-slideRight4"
-          }`}
-        >
-          <span>React</span>
-          <span>Express</span>
-          <span>PostgreSQL</span>
-          <span>Tailwind</span>
-          <span>Prisma</span>
-          <span>AWS S3</span>
-          <span>Radix UI</span>
-        </div>
-        <div
-          className={`flex gap-3 justify-end mt-3 ${
-            second && secondCount === 0 && "animate-slideRight5"
-          }`}
-        >
-          <a href="https://github.com/0205Ankit/userLog" target="_blank">
-            <LuGithub className="text-gray-300 hover:text-violet-500 text-2xl" />
-          </a>
-          <a href="https://user-log.vercel.app/" target="_blank">
-            <LuLink className="text-gray-300 hover:text-violet-500 text-2xl" />
-          </a>
         </div>
       </div>
     </div>
