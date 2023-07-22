c1ff16765a473bf95e15c6fdf054740d0a65fd16
ankit <ankitbisht0205@gmail.com>
2023-07-22 23:40:01 +0530

    styles fix

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -9,7 +9,7 @@ export default function Loading() {
         <div className="relative">
           <div
             className={`text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-600 to-pink-500  text-xl font-extrabold flex flex-col gap-5 top-[5px] animate-cycle 
-          tracking-[2.5px]`}
+          tracking-[2.5px] max-md:tracking-[1.5px]`}
           >
             <span>Javascript</span>
             <span>React</span>
