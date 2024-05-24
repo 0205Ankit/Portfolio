38b00c1610363a18e4a08696398c3ec4d2403cb9
ankit <ankitbisht0205@gmail.com>
2024-05-24 13:02:43 +0530

    updating

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,12 +1,82 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useInViewport } from "react-in-viewport";
 import ButtonMailto from "./MailLink";
+import { HiOutlineBadgeCheck } from "react-icons/hi";
 
 export default function Contact() {
   const myRef = useRef();
   const { inViewport, leaveCount } = useInViewport(myRef);
+  const [showModal, setShowModal] = useState(false);
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [errors, setErrors] = useState({
+    name: null,
+    email: null,
+    message: null,
+  });
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setErrors({
+      name: null,
+      email: null,
+      message: null,
+    });
+
+    if (inputs.name.length < 3) {
+      setErrors({
+        ...errors,
+        name: "Name must be at least 3 characters long",
+      });
+      return;
+    }
+
+    if (inputs.email.length < 11) {
+      setErrors({
+        ...errors,
+        email: "Enter a valid email",
+      });
+      return;
+    }
+
+    if (inputs.message.length < 5) {
+      setErrors({
+        ...errors,
+        message: "Message must be at least 5 characters long",
+      });
+      return;
+    }
+
+    setShowModal(true);
+    setInputs({
+      name: "",
+      email: "",
+      message: "",
+    });
+
+    setTimeout(() => {
+      setShowModal(false);
+    }, 5000);
+  };
+
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    e.preventDefault();
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+    setErrors({
+      ...errors,
+      [e.target.name]: null,
+    });
+  };
 
   return (
     <>
@@ -37,12 +107,75 @@ export default function Contact() {
           I’m currently looking for any new opportunities, so get in touch if
           you have something for me, Thanks.
         </p>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={onChange}
+            className="w-full outline-none rounded-md placeholder:text-slate-600 p-3 mt-7"
+          />
+          {errors.name && (
+            <label htmlFor="name" className="text-red-500 text-xs pl-2">
+              {errors.name}
+            </label>
+          )}
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            value={inputs.email}
+            onChange={onChange}
+            className="w-full outline-none rounded-md placeholder:text-slate-600 p-3 mt-7"
+          />
+          {errors.email && (
+            <label htmlFor="email" className="text-red-500 text-xs pl-2">
+              {errors.email}
+            </label>
+          )}
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Message"
+            value={inputs.message}
+            onChange={onChange}
+            className="w-full outline-none rounded-md placeholder:text-slate-600 p-3 mt-7"
+          ></textarea>
+          {errors.message && (
+            <label htmlFor="message" className="text-red-500 text-xs pl-2">
+              {errors.message}
+            </label>
+          )}
+          <button
+            type="submit"
+            className="w-full py-3 mt-7 disabled:brightness-50 disabled:cursor-not-allowed font-medium border-[1.5px] rounded-md border-violet-500 text-violet-500"
+          >
+            Send
+          </button>
+        </form>
+      </div>
+      {/* ///////////////// Success Modal  /////////////////// */}
+      <div className={`${showModal ? "block" : "hidden"}`}>
         <div
-          className={`py-3 w-fit px-5 mt-7 font-medium border-[1.5px] rounded-md border-violet-500 text-violet-500 ${
-            inViewport && leaveCount === 0 && "animate-fadeUp4"
-          }`}
+          onClick={() => setShowModal(false)}
+          className="fixed w-screen h-screen inset-0 backdrop-blur-xl z-[50]"
+        ></div>
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+          }}
+          className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed w-[400px] bg-darkGrey z-[100] rounded-xl p-3 border-box"
         >
-          <ButtonMailto label="Say Hello" mailto="singhankit8066@gmail.com" />
+          <div className="w-full h-full flex flex-col items-center justify-center p-5 rounded-md">
+            <HiOutlineBadgeCheck className="text-violet-500 text-[100px]" />
+            <p className="text-2xl font-bold text-slate-200">
+              Thanks for getting in touch!
+            </p>
+          </div>
         </div>
       </div>
     </>
